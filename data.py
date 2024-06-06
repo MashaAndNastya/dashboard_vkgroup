016013065d0affd2c58d389900ebb9faa4a96fe7
Куренкова Анастасия Алексеевна <aakurenkova_5@edu.hse.ru>
2024-06-06 16:12:44 +0300

    Созданы функции для обработки данных

--- data.py
+++ data.py
@@ -2,73 +2,149 @@ import pandas as pd
 import requests
 import time
 from datetime import datetime
-
-information_for_dataframe = {
-    'id': [],
-    'text': [],
-    'likes': [],
-    'comments': [],
-    'views': [],
-    'reposts': [],
-    'photo': [],
-    'url': [],
-    'date': [],
-    'date_UNIX': []
-}
-
-url_start = 'https://vk.com/nenovijukabachki'
+from cachetools import cached, TTLCache
+cache = TTLCache(maxsize=1000, ttl=300)  # Ограничиваем кэш максимум 1000 записями на 5 минут
+#Изначальные переменные, потом будут передаваться через поле для ввода
+url_start = 'https://vk.com/dimdimychmusic'
 url = url_start.split('/')
-access_token = "0f081c180f081c180f081c18270c1f36aa00f080f081c186aca12d5e53fc3d7df54f2af"
-version = 5.199
+access_token = "vk1.a.zPEtzBOVfFVEnCAaT2cMvW6CYvsFyOFiB8NFFU9GEz-sWujzUYZuA00WoHRpykSBtNni2EkFM4s4xLB_4_CcWk5SjN-pyh0xoe-pH4OO0CxWWzY1fXxsAzYq0dCXwHimF3p_is6GIyH_wvL0yCGd3SFKeBncr_NOpuodwPr7Hr6Zi9YrG8AQqVtp3Jo-jzA_cFS-1WKcAYnA06vt18QxZg"
 domain = url[-1]
+#константные переменные
+version = 5.199
 count = 100
 offset = 0
-
-response = requests.get('https://api.vk.com/method/wall.get',
-                        params={'access_token': access_token,
-                                'v': version,
-                                'domain': domain,
-                                'count': count,
-                                'offset': offset
-                                })
-
-data_start = response.json()
+#получаем id группы через запрос
 response = requests.get('https://api.vk.com/method/utils.resolveScreenName',
                         params={'access_token': access_token,
                                 'screen_name': domain,
-                                'v': version
-                                })
+                                'v': version})
 id_group = response.json()['response']['object_id']
-count_posts = data_start['response']['count']
 
-for i in range(0, count_posts, 100):
+#стартовое и конечное значение в формате unix-времени, тк вводится в fetch_stats только UNIX время
+start_time = '1709286555'
+end_time = '1713174555'
+#перевод во временной промежуток(список) для построения графика
+date_range = pd.date_range(start=pd.to_datetime(start_time, unit='s').date(), end=pd.to_datetime(end_time, unit='s').date())
+
+
+
+
+
+@cached(cache)
+def fetch_vk_data(access_token, version, count, offset):
+    data_dict = {
+        'ID': [],
+        'Text': [],
+        'Likes': [],
+        'Comments': [],
+        'Views': [],
+        'Reposts': [],
+        'URL': [],
+        'Date': [],
+        'Date_UNIX': []
+    }
+
+
+
     response = requests.get('https://api.vk.com/method/wall.get',
                             params={'access_token': access_token,
                                     'v': version,
                                     'domain': domain,
                                     'count': count,
-                                    'offset': offset
-                                    })
-    data = response.json()['response']['items']
-    offset += 100
-    information_for_dataframe['id'].extend([item['id'] for item in data])
-    information_for_dataframe['likes'].extend([item['likes']['count'] for item in data])
-    information_for_dataframe['text'].extend([item['text'] for item in data])
-    information_for_dataframe['comments'].extend([item['comments']['count'] for item in data])
-    information_for_dataframe['views'].extend([item['views']['count'] for item in data])
-    information_for_dataframe['reposts'].extend([item['reposts']['count'] for item in data])
-    information_for_dataframe['photo'].extend([item['attachments'][0]['photo']['sizes'][-1]['url'] if len(item.get('attachments', [])) > 0 else "No photo" for item in data])
-    information_for_dataframe['url'].extend([url_start + "?w=wall-" + str(id_group) +"_"+ str(item['id']) for item in data])
-    from datetime import datetime, timezone
+                                    'offset': offset})
 
-    information_for_dataframe['date'].extend(
-        [datetime.fromtimestamp(item['date'], timezone.utc).strftime('%Y-%m-%d %H:%M:%S') for item in data])
+    data_start = response.json()
+    count_posts = data_start['response']['count']
+    for i in range(0, count_posts, 100):
+        response = requests.get('https://api.vk.com/method/wall.get',
+                                params={'access_token': access_token,
+                                        'v': version,
+                                        'domain': domain,
+                                        'count': count,
+                                        'offset': offset})
+        data = response.json()['response']['items']
+        offset += 100
+        data_dict['ID'].extend([item['id'] for item in data])
+        data_dict['Likes'].extend([item['likes']['count'] for item in data])
+        data_dict['Text'].extend([item['text'] for item in data])
+        data_dict['Comments'].extend([item['comments']['count'] for item in data])
 
-    information_for_dataframe['date_UNIX'].extend([item['date'] for item in data])
-    time.sleep(0.1)
+        for item in data:
+            if 'views' in item:
+                data_dict['Views'].append(item['views']['count'])
+            else:
+                data_dict['Views'].append(None)
+        data_dict['Reposts'].extend([item['reposts']['count'] for item in data])
+        data_dict['URL'].extend([url_start + "?w=wall-" + str(id_group) + "_" + str(item['id']) for item in data])
+        data_dict['Date'].extend([datetime.utcfromtimestamp(item['date']).strftime('%Y-%m-%d') for item in data])
+        data_dict['Date_UNIX'].extend([item['date'] for item in data])
+        time.sleep(0.01)
 
-df = pd.DataFrame(information_for_dataframe)
-print(df.head())
+    df_posts = pd.DataFrame(data_dict)
+    print(df_posts.columns)
+    return df_posts
 
-pd.set_option('display.max_columns', None)
-print(df.head(5))
+@cached(cache)
+def fetch_vk_stats(start_time, end_time, access_token, id_group):
+    # Загрузка всех параметров
+    params = {
+        'access_token': access_token,
+        'group_id': id_group,
+        'timestamp_from': start_time,
+        'timestamp_to': end_time,
+        'v': version
+    }
+
+    # Запрос к апи
+    response = requests.get('https://api.vk.com/method/stats.get', params=params)
+
+    response_data = response.json()['response']
+
+    # Инициализация заготовок
+    # Initialize empty lists to store the extracted data
+    likes, copies, hidden, comment, subscribed, unsubscribed, visitors = [], [], [], [], [], [], []
+    sex_f, sex_m = [], []
+    age_data = {}
+    age_sex_data = {}
+
+    for item in response_data:
+        activity = item.get("activity", {})  # Handle empty "activity" case
+        reach = item.get("reach", {})
+
+        # Extracting activity data
+        likes.append(activity.get("likes", 0))
+        copies.append(activity.get("copies", 0))
+        hidden.append(activity.get("hidden", 0))
+        comment.append(activity.get("comment", 0))
+        subscribed.append(activity.get("subscribed", 0))
+        unsubscribed.append(activity.get("unsubscribed", 0))
+        visitors.append(reach.get("reach", 0))
+
+        # Extracting sex data
+        for sex in reach.get("sex", []):
+            if sex["value"] == "f":
+                sex_f.append(sex["count"])
+            elif sex["value"] == "m":
+                sex_m.append(sex["count"])
+
+        # Extracting age data
+        for age_group in reach.get("age", []):
+            age_data[age_group["value"]] = age_group["count"]
+
+        # Extracting sex-age data
+        for sex_age in reach.get("sex_age", []):
+            age_sex_data[sex_age["value"]] = sex_age["count"]
+
+    # Creating the dataframes
+    activity_df = pd.DataFrame({"likes": likes, "copies": copies, "hidden": hidden, "comment": comment,
+                                "subscribed": subscribed, "unsubscribed": unsubscribed, "visitors": visitors})
+
+    sex_df = pd.DataFrame({"f": sex_f, "m": sex_m})
+    age_df = pd.DataFrame(list(age_data.items()), columns=["age_group", "count"])
+    age_sex_df = pd.DataFrame(list(age_sex_data.items()), columns=["sex_age", "count"])
+    return likes, copies, hidden, comment, subscribed, unsubscribed, visitors, activity_df, sex_df, age_df, age_sex_df
+
+#Вызов функции, запись  в переменные
+likes, copies, hidden, comment, subscribed, unsubscribed, visitors, activity_df, sex_df, age_df, age_sex_df = fetch_vk_stats(start_time, end_time, access_token, id_group)
+df = fetch_vk_data(access_token, version, count, offset)
+
