3855edf65c526e9d3a157b76516eab5f589c3816
Maria Akhmatgaleeva <mdakhmatgaleeva@edu.hse.ru>
2024-06-09 21:20:23 +0300

    ERR callback

--- app.py
+++ app.py
@@ -8,6 +8,130 @@ import pandas as pd
 from data import *
 from datetime import date, datetime, timedelta
 
+# Функции из data.py
+start_time = '1709251200'
+end_time = str(int(datetime.now().timestamp()))
+
+
+def fetch_vk_stats(start_time, end_time, access_token, id_group):
+    # Загрузка всех параметров
+    params = {
+        'access_token': access_token,
+        'group_id': id_group,
+        'timestamp_from': start_time,
+        'timestamp_to': end_time,
+        'v': version
+    }
+
+    # Запрос к апи
+    response = requests.get('https://api.vk.com/method/stats.get', params=params)
+
+    response_data = response.json()['response']
+
+    # Инициализация заготовок
+    likes, copies, hidden, comment, subscribed, unsubscribed, reach1, reach_subscribers, reach_unique_user = [], [], [], [], [], [], [], [], []
+    sex_f, sex_m = [], []
+    age_data = {}
+    age_sex_data = {}
+
+    for item in response_data:
+        activity = item.get("activity", {})
+        reach = item.get("reach", {})
+        likes.append(activity.get("likes", 0))
+        copies.append(activity.get("copies", 0))
+        hidden.append(activity.get("hidden", 0))
+        comment.append(activity.get("comment", 0))
+        subscribed.append(activity.get("subscribed", 0))
+        unsubscribed.append(activity.get("unsubscribed", 0))
+        reach1.append(reach.get("reach", 0))
+        reach_subscribers.append(reach.get("reach_subscribers", 0))
+        reach_unique_user. append(reach1[-1]-reach_subscribers[-1])
+
+        for sex in reach.get("sex", []):
+            if sex["value"] == "f":
+                sex_f.append(sex["count"])
+            elif sex["value"] == "m":
+                sex_m.append(sex["count"])
+
+        for age_group in reach.get("age", []):
+            age_data[age_group["value"]] = age_group["count"]
+
+        for sex_age in reach.get("sex_age", []):
+            age_sex_data[sex_age["value"]] = sex_age["count"]
+
+    sex_df = pd.DataFrame({"f": sex_f, "m": sex_m})
+    age_df = pd.DataFrame(list(age_data.items()), columns=["age_group", "count"])
+    age_sex_df = pd.DataFrame(list(age_sex_data.items()), columns=["sex_age", "count"])
+    return (likes, copies, hidden, comment, subscribed, unsubscribed, reach1, reach_subscribers, reach_unique_user,
+            sex_df, age_df, age_sex_df)
+
+
+# Перевод во временной промежуток(список) для построения графика
+date_range = pd.date_range(start=pd.to_datetime(int(start_time), unit='s').date(), end=pd.to_datetime(int(end_time), unit='s').date())
+
+# Вызов функции, запись  в переменные и постобработка данных
+likes, copies, hidden, comment, subscribed, unsubscribed, reach, reach_subscribers, reach_unique_user, sex_df, age_df, age_sex_df = fetch_vk_stats(start_time, end_time, access_token, id_group)
+count_female = sum(sex_df['f'])
+count_male = sum(sex_df['m'])
+
+# Данные для диаграммы возрастов
+age_12_21 = age_df[(age_df['age_group'] == '12-18')]['count'].values[0]+age_df[(age_df['age_group'] == '18-21')]['count'].values[0]
+age_21_27 = age_df[(age_df['age_group'] == '21-24')]['count'].values[0]+age_df[(age_df['age_group'] == '24-27')]['count'].values[0]
+age_27_30 = age_df[(age_df['age_group'] == '27-30')]['count'].values[0]+age_df[(age_df['age_group'] == '30-35')]['count'].values[0]
+age_35_45 = age_df[(age_df['age_group'] == '35-45')]['count'].values[0]
+age_45_100 = age_df[(age_df['age_group'] == '45-100')]['count'].values[0]
+
+
+# Расчёт ERR
+def calculate_err_mean(likes, copies, comment, reach):
+    err_mean_calculated = ((sum(likes) + sum(copies) + sum(comment)) / sum(reach)) * 100
+    return err_mean_calculated
+
+
+err_mean = calculate_err_mean(likes, copies, comment, reach)
+
+
+def get_text_advice_err(err_mean):
+    if err_mean <= 1:
+        return "По общим стандартам ваш показатель ERR низкий."
+    elif 1 < err_mean <= 3.5:
+        return "По общим стандартам ваш показатель ERR средний, сложно сделать вывод об эффективности вашего сообщества."
+    else:
+        return "Поздравляем! По общим стандартам ваш показатель ERR высокий."
+
+
+# Вычисление самого популярного поста
+df = fetch_vk_data(access_token, version, count, offset)
+
+
+def find_most_popular_post(df, start_time, end_time, like_weight=0.5, view_weight=0.3, comment_weight=0.2):
+    # Преобразование столбца Date_UNIX в числовой тип данных
+    df['Date_UNIX'] = pd.to_numeric(df['Date_UNIX'], errors='coerce')
+
+    # Преобразование start_time и end_time из str в int
+    start_time = int(start_time)
+    end_time = int(end_time)
+
+    # Преобразование даты из UNIX-времени в datetime
+    df['Date'] = pd.to_datetime(df['Date_UNIX'], unit='s')
+
+    # Фильтрация данных по промежутку времени
+    filtered_df = df[(df['Date_UNIX'] >= start_time) & (df['Date_UNIX'] <= end_time)]
+
+    # Вычисление популярности постов
+    filtered_df.loc[:, 'Popularity'] = (
+            df['Likes'] * like_weight + df['Views'] * view_weight + df['Comments'] * comment_weight
+    )
+
+    # Определение самого популярного поста
+    most_popular_post = filtered_df.loc[filtered_df['Popularity'].idxmax()]
+
+    return most_popular_post
+
+
+most_popular_post = find_most_popular_post(df, start_time, end_time, like_weight=0.5, view_weight=0.3, comment_weight=0.2)
+
+
 # Данные для div'ов
 
 # Данные активности: лайки, комментарии, репосты
@@ -18,6 +142,7 @@ data_activity = {
     "Reposts": copies
 }
 
+# Построение dataframe для активности
 df_activity = pd.DataFrame(data_activity)
 df_activity['Unix'] = df_activity['Date'].apply(lambda x: int(x.timestamp()))
 
@@ -37,6 +162,7 @@ data_dynamic = {
     "Reach unique": reach_unique_user
 }
 
+# Построение dataframe для динамики
 df_dynamic = pd.DataFrame(data_dynamic)
 df_dynamic['Unix'] = df_dynamic['Date'].apply(lambda x: int(x.timestamp()))
 
@@ -145,10 +271,10 @@ app.layout = html.Div([
         # ERR сообщества
         html.Div([
             html.P('ERR сообщества'),
-            html.P(text1_err),
+            html.P(err_mean),
             html.P(text_err),
             html.P('Советы:'),
-            html.P('Mama mia mama mama mia mama mia Mama mia mama mama mia mama mia Mama mia mama mama mia mama mia')
+            html.P(get_text_advice_err(err_mean))
         ], id='ERR',
             className='text-container',
             style={'background-color': '#39344a', 'border-radius': '5px', 'grid-column': 'span 3',
@@ -267,23 +393,16 @@ def update_output(start_date, end_date):
     Input('date-picker-range', 'end_date')]
 )
 def update_graph(selected_period, start_date, end_date):
-    df_activity['Date'] = pd.to_datetime(df_activity['Date'])
-
-    filtered_df_activity = df_activity.copy()
-
-    int(datetime.strptime(str(date.today()), "%Y-%m-%d").timestamp())
+    global start_time, end_time
     if selected_period == 'last_week':
         start_time = int((datetime.now() - timedelta(days=7)).timestamp())
         filtered_df_activity = df_activity[df_activity['Unix'] >= start_time]
-
     elif selected_period == 'last_month':
         start_time = int((datetime.now() - timedelta(days=30)).timestamp())
         filtered_df_activity = df_activity[df_activity['Unix'] >= start_time]
-
     elif selected_period == 'all_time':
         start_time = 1709286555
-        filtered_df_activity = df_activity[df_activity['Unix'] >= start_time]
-
+        filtered_df_activity = df_activity
     elif selected_period == 'custom_date' and start_date and end_date:
         start_time = int(datetime.fromisoformat(start_date).timestamp())
         end_time = int(datetime.fromisoformat(end_date).timestamp())
@@ -303,6 +422,94 @@ def update_graph(selected_period, start_date, end_date):
     return fig_activity
 
 
+#ERR
+@app.callback(
+    Output('ERR', 'children'),
+    [Input('radio-items', 'value'),
+     Input('date-picker-range', 'start_date'),
+     Input('date-picker-range', 'end_date')]
+)
+def update_graph(selected_period, start_date, end_date):
+        if selected_period == 'last_week':
+            start_time_selected = int((datetime.now() - timedelta(days=7)).timestamp())
+            end_time_selected = int(datetime.now().timestamp())
+            # err_mean = 'text1'
+        elif selected_period == 'last_month':
+            start_time_selected = int((datetime.now() - timedelta(days=30)).timestamp())
+            end_time_selected = int(datetime.now().timestamp())
+            # err_mean = 'text2'
+        elif selected_period == 'all_time':
+            start_time_selected = 1709286555
+            end_time_selected = int(datetime.now().timestamp())
+            # err_mean = 'text3'
+        elif selected_period == 'custom_date' and start_date and end_date:
+            start_time_selected = int(datetime.fromisoformat(start_date).timestamp())
+            end_time_selected = int(datetime.fromisoformat(end_date).timestamp())
+            # err_mean = 'text4'
+
+        data = fetch_vk_stats(start_time_selected, end_time_selected, access_token, id_group)
+        likes_selected, copies_selected, comment_selected, reach_selected = data[0], data[1], data[3], data[6]
+
+        err_mean_updated = calculate_err_mean(likes_selected, copies_selected, comment_selected, reach_selected)
+        text_advice_err_updated = get_text_advice_err(err_mean_updated)
+
+        return [
+            html.P('ERR сообщества',
+                    style={'color': '#f9f9f9',
+                           'font-size': '24px',
+                           'font-weight': 'bold',
+                           'font-family': 'Montserrat.ttf',
+                           'mardin-bottom': '5px',
+                           'text-align': 'center'}
+                   ),
+            html.P(err_mean_updated,
+                   style={'color': '#f1986c',
+                          'font-size': '34px',
+                          'font-weight': 'bold',
+                          'mardin-top': '5px',
+                          'text-align': 'center'}
+                   ),
+            html.P(text_err,
+                   style={'color': '#f9f9f9',
+                          'font-size': '12px'}
+                   ),
+            html.P('Советы:',
+                   style={'color': '#f9f9f9',
+                          'font-size': '20px',
+                          'font-weight': 'bold'}
+                   ),
+
+            html.P(text_advice_err_updated,
+                   style={'color': '#f9f9f9', 'font-size': '12px'}),
+
+            html.P(
+                """Однако общие стандарты бывают обманчивы, лучший способ оценить
+                привлекательность контента — ежемесячно сравнивать текущее значение с ER за предыдущий период.""",
+                   style={'color': '#f9f9f9', 'font-size': '12px'}),
+            html.P(
+                "Можете воспользоваться следующими советами для повышения ERR:",
+                   style={'color': '#f9f9f9', 'font-size': '12px'}),
+            html.Ul([
+                html.Li(
+                    "Задавайте вопросы: это увеличивает количество комментариев.",
+                        style={'color': '#f9f9f9', 'font-size': '12px'}),
+                html.Li(
+                    "Вводите геймификацию: интерактивы удерживают людей и продлевают взаимодействие с публикацией.",
+                    style={'color': '#f9f9f9', 'font-size': '12px'}),
+                html.Li(
+                    "Проводите опросы и голосования: они хорошо вовлекают.",
+                        style={'color': '#f9f9f9', 'font-size': '12px'}),
+                html.Li(
+                    "Общайтесь в комментариях: отвечайте на сообщения — так люди будут втягиваться в общение с брендом.",
+                    style={'color': '#f9f9f9', 'font-size': '12px'}),
+                html.Li(
+                    "Оптимизируйте базу подписчиков.",
+                        style={'color': '#f9f9f9', 'font-size': '12px'}),
+            ])
+        ]
+
+
+
 # User_dynamic
 @app.callback(
     Output('users_dynamic', 'figure'),
@@ -311,36 +518,30 @@ def update_graph(selected_period, start_date, end_date):
     Input('date-picker-range', 'end_date')]
 )
 def update_graph(selected_period, start_date, end_date):
-    df_dynamic['Date'] = pd.to_datetime(df_dynamic['Date'])
-
-    filtered_df_dynamic = df_dynamic.copy()
-
-    int(datetime.strptime(str(date.today()), "%Y-%m-%d").timestamp())
+    global start_time, end_time
     if selected_period == 'last_week':
         start_time = int((datetime.now() - timedelta(days=7)).timestamp())
-        filtered_df_dynamic = df_dynamic[df_dynamic['Unix'] >= start_time]
 
     elif selected_period == 'last_month':
         start_time = int((datetime.now() - timedelta(days=30)).timestamp())
-        filtered_df_dynamic = df_dynamic[df_dynamic['Unix'] >= start_time]
 
     elif selected_period == 'all_time':
         start_time = 1709286555
-        filtered_df_dynamic = df_dynamic[df_dynamic['Unix'] >= start_time]
 
     elif selected_period == 'custom_date' and start_date and end_date:
         start_time = int(datetime.fromisoformat(start_date).timestamp())
         end_time = int(datetime.fromisoformat(end_date).timestamp())
-        filtered_df_dynamic = df_dynamic[(df_dynamic['Unix'] >= start_time) & (df_dynamic['Unix'] <= end_time)]
 
     fig_dynamic = go.Figure()
-    fig_dynamic.add_trace(go.Scatter(x=filtered_df_dynamic['Date'], y=filtered_df_dynamic['Reach subscribers'], mode='lines+markers', name='Reach subscribers'))
-    fig_dynamic.add_trace(go.Scatter(x=filtered_df_dynamic['Date'], y=filtered_df_dynamic['Reach unique'], mode='lines+markers', name='Reach unique'))
+    fig_dynamic.add_trace(go.Scatter(x=df_dynamic['Date'], y=df_dynamic['Reach subscribers'], mode='lines+markers', name='Reach subscribers'))
+    fig_dynamic.add_trace(go.Scatter(x=df_dynamic['Date'], y=df_dynamic['Reach unique'], mode='lines+markers', name='Reach unique'))
 
     fig_dynamic.update_layout(title='User Dynamics', xaxis_title='Date', yaxis_title='Count')
     fig_dynamic.update_layout(plot_bgcolor='#39344a', paper_bgcolor='#39344a', font_color='#cbc2b9')
 
     return fig_dynamic
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
 
