b33f45a991fcbf96f6fb4dc5854313708d818bc6
Куренкова Анастасия Алексеевна <aakurenkova_5@edu.hse.ru>
2024-06-11 12:50:22 +0300

    вывод карточки самого популярного поста + топ целевой аудитории, пофикшена проблема с временем

--- data.py
+++ data.py
@@ -10,19 +10,15 @@ url = url_start.split('/')
 access_token = "vk1.a.zPEtzBOVfFVEnCAaT2cMvW6CYvsFyOFiB8NFFU9GEz-sWujzUYZuA00WoHRpykSBtNni2EkFM4s4xLB_4_CcWk5SjN-pyh0xoe-pH4OO0CxWWzY1fXxsAzYq0dCXwHimF3p_is6GIyH_wvL0yCGd3SFKeBncr_NOpuodwPr7Hr6Zi9YrG8AQqVtp3Jo-jzA_cFS-1WKcAYnA06vt18QxZg"
 domain = url[-1]
 
-#константные переменные
-version = 5.199
-count = 100
-offset = 0
 
 #получаем id группы через запрос
 response = requests.get('https://api.vk.com/method/utils.resolveScreenName',
                         params={'access_token': access_token,
                                 'screen_name': domain,
-                                'v': version})
+                                'v': 5.199})
 id_group = response.json()['response']['object_id']
 
-def fetch_vk_data(access_token, version, count, offset):
+def fetch_vk_data(access_token, version = 5.199 , count = 100, offset = 0):
 
     data_dict = {
         'ID': [],
@@ -33,13 +29,14 @@ def fetch_vk_data(access_token, version, count, offset):
         'Reposts': [],
         'URL': [],
         'Date': [],
-        'Date_UNIX': []
+        'Date_UNIX': [],
+        'Photo': []
     }
 
 
     response = requests.get('https://api.vk.com/method/wall.get',
                             params={'access_token': access_token,
-                                    'v': version,
+                                    'v': 5.199,
                                     'domain': domain,
                                     'count': count,
                                     'offset': offset})
@@ -69,10 +66,14 @@ def fetch_vk_data(access_token, version, count, offset):
         data_dict['URL'].extend([url_start + "?w=wall-" + str(id_group) + "_" + str(item['id']) for item in data])
         data_dict['Date'].extend([datetime.fromtimestamp(item['date'], timezone.utc).strftime('%Y-%m-%d') for item in data])
         data_dict['Date_UNIX'].extend([item['date'] for item in data])
+        for item in data:
+            if 'attachments' in item and item['attachments'] and item['attachments'][0]['type'] == 'photo':
+                data_dict['Photo'].append(item['attachments'][0]['photo']['sizes'][-1]['url'])
+            else:
+                data_dict['Photo'].append("No photo")
         time.sleep(0.01)
 
     df_posts = pd.DataFrame(data_dict)
-    # print(df_posts.columns)
     return df_posts
 
 
